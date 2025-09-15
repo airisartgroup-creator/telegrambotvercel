c1ecf9ac143990e9b627b0c5feed5b6afc005a55
airisartgroup-creator <airisartgroup@gmail.com>
2025-09-15 06:54:49 +0400

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const TOKEN = process.env.BOT_TOKEN;
-  const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+  const TELEGRAM_API = `https://api.telegram.org/bot${7501137487:AAH7W3Cm6iivjoCBTvaS1-wAOFun85jf8cc}`;
 
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
